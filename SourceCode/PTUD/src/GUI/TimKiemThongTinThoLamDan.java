8f426233b0780adc54be3e3fc817b8b9e8601250
Hà Mạnh Hưng <hungha.pn213@gmail.com>
2023-10-26 20:53:59 +0700

    tld

--- SourceCode/PTUD/src/GUI/TimKiemThongTinThoLamDan.java
+++ SourceCode/PTUD/src/GUI/TimKiemThongTinThoLamDan.java
@@ -5,7 +5,6 @@ import java.awt.Dimension;
 import java.awt.Font;
 
 import javax.swing.JButton;
-import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
@@ -15,7 +14,6 @@ import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.JTableHeader;
-import java.awt.GridLayout;
 import javax.swing.SwingConstants;
 
 public class TimKiemThongTinThoLamDan extends JPanel {
@@ -31,12 +29,19 @@ public class TimKiemThongTinThoLamDan extends JPanel {
 	public TimKiemThongTinThoLamDan() {
 		setLayout(null);
 		setBackground(new Color(221, 242, 251));
-	    
-		JLabel lblNewLabel = new JLabel("New label");
-	    lblNewLabel.setBounds(325, 5, 800, 70);
-	    lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 40));
-	    add(lblNewLabel);
-	    lblNewLabel.setText("TÌM KIẾM THÔNG TIN THỢ LÀM ĐÀN");
+		
+		JLabel lblNewLabel = new JLabel("TÌM KIẾM THÔNG TIN THỢ LÀM ĐÀN");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 40));
+		lblNewLabel.setForeground(new Color(0, 27, 72));
+		lblNewLabel.setBounds(10, 5, 1430, 70);
+		add(lblNewLabel);
+		
+		JPanel panel_2 = new JPanel();
+	    panel_2.setBackground(Color.WHITE);
+	    panel_2.setBounds(15, 490, 1420, 200);
+	    add(panel_2);
+	    panel_2.setLayout(null);
 	    
 	    JSeparator separator_1 = new JSeparator();
 	    separator_1.setBackground(Color.BLACK);
@@ -44,17 +49,12 @@ public class TimKiemThongTinThoLamDan extends JPanel {
 	    separator_1.setBounds(15, 430, 1420, 3);
 	    add(separator_1);
 	    
-	    JPanel panel_2 = new JPanel();
-	    panel_2.setBackground(Color.WHITE);
-	    panel_2.setBounds(15, 490, 1420, 200);
-	    add(panel_2);
-	    panel_2.setLayout(null);
-	    
 	    JScrollPane scrollPane_1 = new JScrollPane();
 	    scrollPane_1.setBounds(20, 10, 1380, 180);
 	    panel_2.add(scrollPane_1);
 	    
-	    JTable table_1 = new JTable();
+
+		JTable table_1 = new JTable();
 	    scrollPane_1.setViewportView(table_1);
 	    JTableHeader tb1 = table_1.getTableHeader();
 	    tb1.setBackground(new Color(221, 242, 251));
@@ -89,7 +89,7 @@ public class TimKiemThongTinThoLamDan extends JPanel {
 	    add(panel);
 	    panel.setLayout(null);
 	    
-	    JLabel lblNewLabel_6_1 = new JLabel("Tìm kiếm thợ làm đàn theo:");
+	    JLabel lblNewLabel_6_1 = new JLabel("Tìm kiếm thợ làm đàn theo :");
 	    lblNewLabel_6_1.setFont(new Font("Tahoma", Font.BOLD, 20));
 	    lblNewLabel_6_1.setBounds(35, 15, 304, 25);
 	    panel.add(lblNewLabel_6_1);
@@ -98,19 +98,19 @@ public class TimKiemThongTinThoLamDan extends JPanel {
 	    btnTmKim.setForeground(Color.WHITE);
 	    btnTmKim.setFont(new Font("Tahoma", Font.BOLD, 12));
 	    btnTmKim.setBackground(new Color(2, 104, 156));
-	    btnTmKim.setBounds(499, 237, 170, 50);
+	    btnTmKim.setBounds(499, 255, 170, 50);
 	    panel.add(btnTmKim);
 	    
 	    JButton btnXoRng = new JButton("Xoá rỗng");
 	    btnXoRng.setForeground(Color.WHITE);
 	    btnXoRng.setFont(new Font("Tahoma", Font.BOLD, 12));
 	    btnXoRng.setBackground(new Color(2, 104, 156));
-	    btnXoRng.setBounds(803, 237, 170, 50);
+	    btnXoRng.setBounds(803, 255, 170, 50);
 	    panel.add(btnXoRng);
 	    
 	    JPanel panel_1 = new JPanel();
 	    panel_1.setBackground(new Color(255, 255, 255));
-	    panel_1.setBounds(91, 65, 560, 25);
+	    panel_1.setBounds(50, 65, 519, 25);
 	    panel.add(panel_1);
 	    panel_1.setLayout(null);
 	    
@@ -128,6 +128,9 @@ public class TimKiemThongTinThoLamDan extends JPanel {
 	    textField = new JTextField();
 	    textField.setEditable(false);
 	    textField.setBounds(180, 0, 380, 25);
+	    
+	    textField = new JTextField();
+	    textField.setBounds(201, 0, 318, 25);
 	    panel_1.add(textField);
 	    textField.setColumns(10);
 	    
@@ -135,6 +138,7 @@ public class TimKiemThongTinThoLamDan extends JPanel {
 	    panel_1_1.setLayout(null);
 	    panel_1_1.setBackground(Color.WHITE);
 	    panel_1_1.setBounds(91, 122, 560, 25);
+	    panel_1_1.setBounds(50, 134, 519, 25);
 	    panel.add(panel_1_1);
 	    
 	    JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("");
@@ -142,7 +146,6 @@ public class TimKiemThongTinThoLamDan extends JPanel {
 	    rdbtnNewRadioButton_1.setBackground(Color.WHITE);
 	    rdbtnNewRadioButton_1.setBounds(0, 0, 25, 25);
 	    panel_1_1.add(rdbtnNewRadioButton_1);
-	    
 	    JLabel lblNewLabel_1_1 = new JLabel("CMND:");
 	    lblNewLabel_1_1.setFont(new Font("Tahoma", Font.BOLD, 16));
 	    lblNewLabel_1_1.setBounds(31, 0, 140, 25);
@@ -151,12 +154,17 @@ public class TimKiemThongTinThoLamDan extends JPanel {
 	    textField_1 = new JTextField();
 	    textField_1.setColumns(10);
 	    textField_1.setBounds(180, 0, 380, 25);
+	    
+	    textField_1 = new JTextField();
+	    textField_1.setColumns(10);
+	    textField_1.setBounds(201, 0, 318, 25);
 	    panel_1_1.add(textField_1);
 	    
 	    JPanel panel_1_2 = new JPanel();
 	    panel_1_2.setLayout(null);
 	    panel_1_2.setBackground(Color.WHITE);
 	    panel_1_2.setBounds(759, 65, 560, 25);
+	    panel_1_2.setBounds(800, 65, 519, 25);
 	    panel.add(panel_1_2);
 	    
 	    JRadioButton rdbtnNewRadioButton_2 = new JRadioButton("");
@@ -165,20 +173,25 @@ public class TimKiemThongTinThoLamDan extends JPanel {
 	    rdbtnNewRadioButton_2.setBounds(0, 0, 25, 25);
 	    panel_1_2.add(rdbtnNewRadioButton_2);
 	    
-	    JLabel lblNewLabel_1_2 = new JLabel("Họ tên:");
-	    lblNewLabel_1_2.setFont(new Font("Tahoma", Font.BOLD, 16));
-	    lblNewLabel_1_2.setBounds(31, 0, 140, 25);
-	    panel_1_2.add(lblNewLabel_1_2);
+	    JLabel lblNewLabel_1_22 = new JLabel("Họ tên:");
+	    lblNewLabel_1_22.setFont(new Font("Tahoma", Font.BOLD, 16));
+	    lblNewLabel_1_22.setBounds(31, 0, 140, 25);
+	    panel_1_2.add(lblNewLabel_1_22);
 	    
 	    textField_2 = new JTextField();
 	    textField_2.setColumns(10);
 	    textField_2.setBounds(180, 0, 380, 25);
+	    
+	    textField_2 = new JTextField();
+	    textField_2.setColumns(10);
+	    textField_2.setBounds(201, 0, 318, 25);
 	    panel_1_2.add(textField_2);
 	    
 	    JPanel panel_1_3 = new JPanel();
 	    panel_1_3.setLayout(null);
 	    panel_1_3.setBackground(Color.WHITE);
 	    panel_1_3.setBounds(759, 122, 560, 25);
+	    panel_1_3.setBounds(800, 134, 519, 25);
 	    panel.add(panel_1_3);
 	    
 	    JRadioButton rdbtnNewRadioButton_3 = new JRadioButton("");
@@ -199,42 +212,65 @@ public class TimKiemThongTinThoLamDan extends JPanel {
 	    
 	    JPanel panel_1_4 = new JPanel();
 	    panel_1_4.setBackground(Color.WHITE);
-	    panel_1_4.setBounds(446, 179, 560, 25);
+	    panel_1_4.setBounds(448, 202, 560, 25);
 	    panel.add(panel_1_4);
 	    panel_1_4.setLayout(null);
 	    
 	    JRadioButton rdbtnNewRadioButton_6 = new JRadioButton("");
+	    rdbtnNewRadioButton_6.setBounds(1, 0, 25, 25);
 	    rdbtnNewRadioButton_6.setBackground(new Color(255, 255, 255));
 	    rdbtnNewRadioButton_6.setHorizontalAlignment(SwingConstants.CENTER);
-	    rdbtnNewRadioButton_6.setBounds(1, 0, 25, 25);
 	    panel_1_4.add(rdbtnNewRadioButton_6);
 	    
 	    JLabel lblNewLabel_4 = new JLabel("Giới tính:");
+	    lblNewLabel_4.setBounds(32, 0, 85, 25);
 	    lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD, 16));
-	    lblNewLabel_4.setBounds(32, 0, 93, 25);
 	    panel_1_4.add(lblNewLabel_4);
 	    
 	    JRadioButton rdbtnNewRadioButton_5 = new JRadioButton("");
+	    rdbtnNewRadioButton_5.setBounds(225, 0, 25, 25);
 	    rdbtnNewRadioButton_5.setSelected(true);
 	    rdbtnNewRadioButton_5.setBackground(new Color(255, 255, 255));
 	    rdbtnNewRadioButton_5.setHorizontalAlignment(SwingConstants.CENTER);
-	    rdbtnNewRadioButton_5.setBounds(187, 0, 25, 25);
 	    panel_1_4.add(rdbtnNewRadioButton_5);
 	    
 	    JLabel lblNewLabel_3 = new JLabel("Nam");
+	    lblNewLabel_3.setBounds(256, 0, 85, 25);
 	    lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 16));
-	    lblNewLabel_3.setBounds(218, 0, 93, 25);
 	    panel_1_4.add(lblNewLabel_3);
+	    textField_3.setBounds(201, 0, 318, 25);
+	    panel_1_3.add(textField_3);
+	    
+	    JRadioButton rdbtnNewRadioButton_66 = new JRadioButton("");
+	    rdbtnNewRadioButton_66.setHorizontalAlignment(SwingConstants.CENTER);
+	    rdbtnNewRadioButton_66.setBackground(new Color(255, 255, 255));
+	    rdbtnNewRadioButton_66.setBounds(55, 0, 25, 25);
+	    
+	    JLabel lblNewLabel_44 = new JLabel("Giới tính: ");
+	    lblNewLabel_44.setFont(new Font("Tahoma", Font.BOLD, 16));
+	    
+	    JRadioButton rdbtnNewRadioButton_55 = new JRadioButton("");
+	    rdbtnNewRadioButton_55.setBackground(new Color(255, 255, 255));
+	    rdbtnNewRadioButton_55.setHorizontalAlignment(SwingConstants.CENTER);
+	    
+	    JLabel lblNewLabel_33 = new JLabel("Nam");
+	    lblNewLabel_33.setFont(new Font("Tahoma", Font.BOLD, 16));
 	    
 	    JRadioButton rdbtnNewRadioButton_4 = new JRadioButton("");
 	    rdbtnNewRadioButton_4.setBackground(new Color(255, 255, 255));
 	    rdbtnNewRadioButton_4.setHorizontalAlignment(SwingConstants.CENTER);
 	    rdbtnNewRadioButton_4.setBounds(373, 0, 25, 25);
-	    panel_1_4.add(rdbtnNewRadioButton_4);
 	    
 	    JLabel lblNewLabel_2 = new JLabel("Nữ");
-	    lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 16));
 	    lblNewLabel_2.setBounds(404, 0, 93, 25);
+	    lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 16));
 	    panel_1_4.add(lblNewLabel_2);
+	    
+	    JRadioButton rdbtnNewRadioButton_6_1 = new JRadioButton("");
+	    rdbtnNewRadioButton_6_1.setHorizontalAlignment(SwingConstants.CENTER);
+	    rdbtnNewRadioButton_6_1.setBackground(Color.WHITE);
+	    rdbtnNewRadioButton_6_1.setBounds(373, 0, 25, 25);
+	    panel_1_4.add(rdbtnNewRadioButton_6_1);
+	    rdbtnNewRadioButton_4.setBounds(395, 0, 25, 25);
 	}
 }
